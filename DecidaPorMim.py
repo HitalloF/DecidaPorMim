76300cedf861aa6215cd46bb10452c988a25f561
HitalloF <hitalloufpe@gmail.com>
2022-01-10 22:12:46 -0300

    First Commit

---
+++ DecidaPorMim.py
@@ -0,0 +1,11 @@
+import random
+import  PySimpleGUI as sg
+
+class DecidaPorMin:
+    def __init__(self):
+        self.respostas = [  z
+            'yeah, you need it',
+            'i dont know',
+            'dont make it',
+            'Not yet'
+        ]
